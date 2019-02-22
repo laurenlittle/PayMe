897151e6e2eb45d2a4f29fee5f36ea7ac870b657
laurenlittle <littlelauren016@gmail.com>
2019-02-21 22:56:10 -0500

    updated main/welcome page styles

--- src/components/GetStarted/GetStarted.js
+++ src/components/GetStarted/GetStarted.js
@@ -4,8 +4,17 @@ import './GetStarted.css';
 class GetStarted extends React.Component {
   render() {
     return(
-      <div>
-        <h1>Get Started Today with PayMe</h1>
+      <div className="get-started">
+        <h1 className="welcome-message-header">Welcome to PayMe</h1>
+        <h3>Take Control of your Finances.</h3>
+        <p>Be on top of your money.</p>
+        <p>Create a budget.</p>
+        <p>Receive alerts when bills are due.</p>
+
+        <div className="button-wrapper">
+          <button className="sign-in-btn">Sign In</button>
+          <button className="sign-up-btn">Sign Up</button>
+        </div>
       </div>
     );
   }
